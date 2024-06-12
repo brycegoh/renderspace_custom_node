dc516ccb92e91f20ce056f8242ffc6ab95c1c341
brycegoh <brycegoh96@gmail.com>
2024-06-12 15:10:27 +0800

    asd

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-# bash cmd to move custom_nodes/renderspace to ../
-mv custom_nodes/renderspace .
-mv web/extensions/renderspace ../web/extensions/
+mv custom_nodes/renderspace ../
+mv web/extensions/renderspace ../../web/extensions/
